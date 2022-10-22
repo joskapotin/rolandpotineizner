1d430074bbd5d695b43b527be2f2e655933959e5
joska potin <jpotin@gmail.com>
2022-10-22 14:07:30 +0200

    remove scroll to top

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react"
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import Footer from "../footer/footer"
 import Header from "../header/header"
 import OrnateLineBreakSvg from "../svg/ornate-line-break-svg"
 
 function Layout() {
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth", // Optional if you want to skip the scrolling animation
-    })
-  }, [pathname])
-
   return (
     <>
       <Header />
