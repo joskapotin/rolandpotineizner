7708775819cf4c017c64ee8faacd5b0db227fdca
joska potin <jpotin@gmail.com>
2022-10-25 20:38:13 +0200

    center image

--- src/components/blurhash/blurhash.tsx
+++ src/components/blurhash/blurhash.tsx
@@ -19,7 +19,7 @@ function Blurhash(props: BlurhashProps) {
   }
 
   return (
-    <div className="isolate grid">
+    <div className="isolate grid justify-items-center">
       <img
         className={`col-span-full row-span-full h-auto w-max object-cover object-center transition-opacity duration-500 ease-in-out ${
           isLoaded ? "opacity-100" : "opacity-0"
